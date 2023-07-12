1d9ccb61187d7f368c5112c7d833163ff7c202a1
AbhishekAryanWeb170901 <bittoogoswami@gmail.com>
2023-07-12 21:25:53 +0530

    oddeve

---
+++ 1.Maths/src/practice/d1/$2OddEveTernary.java
@@ -0,0 +1,11 @@
+package practice.d1;
+
+//check given no is odd or even using ternary opr
+public class $2OddEveTernary {
+public static void main(String[] args) {
+	int x=21;
+	//using ternary opr
+	String y = ((x%2)==0)? "Even":"Odd";
+	System.out.println(y);
+}
+}
