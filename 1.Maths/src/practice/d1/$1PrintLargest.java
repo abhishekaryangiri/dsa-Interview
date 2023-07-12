5c00575e963abd11277f1305b9d0db25e6d4ad65
AbhishekAryanWeb170901 <bittoogoswami@gmail.com>
2023-07-12 21:19:16 +0530

    maths

--- 1.Maths/src/practice/d1/$1PrintLargest.java
+++ 1.Maths/src/practice/d1/$1PrintLargest.java
@@ -1,5 +1,17 @@
 package practice.d1;
 
 public class $1PrintLargest {
-
+	public static void main(String[] args) {
+		int x=20,y=30,z=50;
+		if(x>=y && x>=z) {
+			System.out.println("X: Larger");
+		}
+		else if( y>=x && y>=z) {
+			System.out.println("Y: Larger");
+		}
+		else {
+			System.out.println("Z: Larger");
+		}
+		
+	}
 }
