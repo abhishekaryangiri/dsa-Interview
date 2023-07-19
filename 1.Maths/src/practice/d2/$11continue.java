0c69f11a16784a508b356f3569cebbcc3f1143c9
AbhishekAryanWeb170901 <bittoogoswami@gmail.com>
2023-07-19 23:07:25 +0530

    pattern

---
+++ 1.Maths/src/practice/d2/$11continue.java
@@ -0,0 +1,13 @@
+package practice.d2;
+
+public class $11continue {
+public static void main(String[] args) {
+	//continue: skip
+	for( int j=1;j<=9;j++) {
+			if(j==6) {
+				continue;
+			}
+			System.out.print(j+ " ");
+		}
+}
+}
