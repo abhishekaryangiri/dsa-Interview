ab8229467f0fd2741da902d01cfc099e9d92c5e2
AbhishekAryanWeb170901 <bittoogoswami@gmail.com>
2023-07-21 22:51:21 +0530

    tri

--- 1.Maths/src/practice/d3NestedLoop/$4Trangle.java
+++ 1.Maths/src/practice/d3NestedLoop/$4Trangle.java
@@ -2,13 +2,14 @@ package practice.d3NestedLoop;
 
 public class $4Trangle {
 public static void main(String[] args) {
-	int a=8;
+	int a=5;
 	//out Loop
 	for( int i=1;i<=a;i++) {
 		//in Loop
 		for( int j=1;j<=i;j++) {
-			
+			System.out.print("$");
 		}
+		System.out.println();
 	}
 }
 }
