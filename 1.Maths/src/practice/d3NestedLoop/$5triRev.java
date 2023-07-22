0e00f2ea12926964e195bd11baf3b4badc6d4bfa
AbhishekAryanWeb170901 <bittoogoswami@gmail.com>
2023-07-22 23:48:10 +0530

    rev tri.

--- 1.Maths/src/practice/d3NestedLoop/$5triRev.java
+++ 1.Maths/src/practice/d3NestedLoop/$5triRev.java
@@ -1,9 +1,14 @@
 package practice.d3NestedLoop;
 
 public class $5triRev {
-int x=6;
-for( int a=x;a>=1;a--) {
-	//inn loop
-	
+public static void main(String[] args) {
+	int x=6;
+	for( int a=x;a>=1;a--) {
+		//inn loop
+		for(int b=1;b<=x;b++) {
+			System.out.print("$");
+		}
+		System.out.println();
+	}
 }
 }
