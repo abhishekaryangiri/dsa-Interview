d113a993f6efd398ca2fc1962f38926caadd2798
AbhishekAryanWeb170901 <bittoogoswami@gmail.com>
2023-07-24 23:48:54 +0530

    pat..

--- 1.Maths/src/practice/d3NestedLoop/$8Pat5.java
+++ 1.Maths/src/practice/d3NestedLoop/$8Pat5.java
@@ -1,5 +1,14 @@
 package practice.d3NestedLoop;
 
 public class $8Pat5 {
-
+public static void main(String[] args) {
+	int x=6;
+	//out lp
+	for(int a=1;a<=x;a++) {
+		//in ;p
+		for(int b=1;b<=x-1+1;b++) {
+			
+		}
+	}
+}
 }
