4b8b0ae50afe578fcb098627b820425a0aa95fe3
AbhishekAryanWeb170901 <bittoogoswami@gmail.com>
2023-07-25 12:28:41 +0530

    palindrom

---
+++ 1.Maths/src/interviewproblems/PalindromeFinder.java
@@ -0,0 +1,42 @@
+package interviewproblems;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class PalindromeFinder {
+public static void main(String[] args) {
+    String[] array = {"madam", "car", "aaa", "radar", "dog"};
+    List<String> palindromes = findPalindromes(array);
+    System.out.println("Given Array: "+Arrays.asList(array));
+    System.out.println("Possible palindromes in the given array: " + palindromes);
+}
+
+public static List<String> findPalindromes(String[] array) {
+    List<String> palindromes = new ArrayList<>();
+
+    for (String str : array) {
+        if (isPalindrome(str)) {
+            palindromes.add(str);
+        }
+    }
+
+    return palindromes;
+}
+
+public static boolean isPalindrome(String str) {
+    int left = 0;
+    int right = str.length() - 1;
+
+    while (left < right) {
+        if (str.charAt(left) != str.charAt(right)) {
+            return false;
+        }
+        left++;
+        right--;
+    }
+
+    return true;
+}
+}
