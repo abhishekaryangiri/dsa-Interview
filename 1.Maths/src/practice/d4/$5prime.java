ba7f67b1abce116a28139d362945e229d6bf9aef
AbhishekAryanWeb170901 <bittoogoswami@gmail.com>
2023-08-07 23:59:49 +0530

    p

--- 1.Maths/src/practice/d4/$5prime.java
+++ 1.Maths/src/practice/d4/$5prime.java
@@ -1,5 +1,18 @@
 package practice.d4;
 
 public class $5prime {
-
+	public static boolean checkPrim(int x) {
+		boolean checkPrim=true;
+		for( int a=2; a<=x-1; x++) {
+			if(x%a==0) {
+				checkPrim=false;
+				break;
+			}
+		}
+		return checkPrim;
+	}
+	public static void main(String[] args) {
+		checkPrim(12);
+	}
+	
 }
