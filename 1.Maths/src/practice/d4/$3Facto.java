6bd7b3f84af59fba981672684869665397d6cf95
AbhishekAryanWeb170901 <bittoogoswami@gmail.com>
2023-08-07 18:22:44 +0530

    fac

--- 1.Maths/src/practice/d4/$3Facto.java
+++ 1.Maths/src/practice/d4/$3Facto.java
@@ -2,4 +2,15 @@ package practice.d4;
 
 public class $3Facto {
 
+	public static int facto(int n) {
+		int f = 1;
+		for(int x=1; x<=n; x++) {
+			f = f*x;
+		}
+		return f;
+	}
+	public static void main(String[] args) {
+		int r = facto(5);
+		System.out.println(r);
+	}
 }
