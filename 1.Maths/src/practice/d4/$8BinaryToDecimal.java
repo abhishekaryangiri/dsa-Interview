efb39ea2a74f353f80baddfb66669369e81ba7b8
AbhishekAryanWeb170901 <bittoogoswami@gmail.com>
2023-08-08 16:14:15 +0530

    decitobin

---
+++ 1.Maths/src/practice/d4/$8BinaryToDecimal.java
@@ -0,0 +1,23 @@
+package practice.d4;
+
+public class $8BinaryToDecimal {
+	public static int binaryToDeci(String binary) {
+		int deci = 0;
+		int base = 1;
+		
+		for( int x=binary.length()-1; x>=0; x--) {
+			if(binary.charAt(x)=='1') {
+				deci += base;
+			}
+			base *= 2;
+		}
+		return deci;
+	}
+	public static void main(String[] args) {
+		String input = "1101";
+		int bn = binaryToDeci(input);
+		System.out.println("Binary: "+ input);
+		System.out.println("Decimal: "+bn);
+	}
+
+}
---
+++ 1.Maths/src/practice/d4/$9DecimalToBinary.java
@@ -0,0 +1,24 @@
+package practice.d4;
+
+public class $9DecimalToBinary {
+	public static String deciToBinary(int deci) {
+		if(deci == 0) {
+			return "0"; //for spl case like 0
+		}
+		StringBuilder binary = new StringBuilder();
+		
+		while(deci > 0) {
+			int reminder = deci % 2;
+			binary.insert(0, reminder);
+			deci /= 2;
+		}
+		return binary.toString();
+	}
+	public static void main(String[] args) {
+		int dn = 25;
+		String bn = deciToBinary(dn);
+		System.out.println("Decimal: "+dn);
+		System.out.println("Binary: "+bn);
+	}
+
+}
