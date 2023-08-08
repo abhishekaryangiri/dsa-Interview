2a036714f9c8886a1c13b0f9883b3c415b9bc416
AbhishekAryanWeb170901 <bittoogoswami@gmail.com>
2023-08-08 14:46:33 +0530

    allPri

---
+++ 1.Maths/src/practice/d4/$6PerfectSqr.java
@@ -0,0 +1,17 @@
+package practice.d4;
+
+public class $6PerfectSqr {
+	public static boolean checkPerfectSqr(int num) {
+		if(num<0) {
+			return false;
+		}
+		 int sqr =(int) Math.sqrt(num);
+		return  sqr*sqr == num;
+	}
+	public static void main(String[] args) {
+		int num = 100;	 
+		boolean check = checkPerfectSqr(num);
+		System.out.println(check);
+	}
+
+}
