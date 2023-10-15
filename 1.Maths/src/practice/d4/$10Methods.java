5ae9e88f6ccfa353cd480d18af1af0a7699353dc
AbhishekAryanWeb170901 <bittoogoswami@gmail.com>
2023-10-15 23:30:13 +0530

    update to find prime no again

--- 1.Maths/src/practice/d4/$10Methods.java
+++ 1.Maths/src/practice/d4/$10Methods.java
@@ -28,15 +28,17 @@ public class $10Methods {
 	}
 	// method to find prime no:
 	static boolean isPrime(int n) {
-		boolean isPrime=true;
+		//boolean isPrime=true;
 		for(int i=2;i<=n-1;i++) {
 			if(n%i==0) {
-				isPrime= false;
-				break;
+				//isPrime= false;
+				return false;
+				//break;
 			}
 			
 		}
-		return isPrime;
+		//return isPrime;
+		return true;
 	}
 	public static void main(String[] args) {
 		
