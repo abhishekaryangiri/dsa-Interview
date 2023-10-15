3e91e5d4e7a4ed6d9a7594e836931412aef7d7ad
AbhishekAryanWeb170901 <bittoogoswami@gmail.com>
2023-10-15 22:30:57 +0530

    update method

--- 1.Maths/src/practice/d4/$10Methods.java
+++ 1.Maths/src/practice/d4/$10Methods.java
@@ -2,4 +2,13 @@ package practice.d4;
 
 public class $10Methods {
 
+	/*
+	Methods are of 2 types:-
+	1. User-defined
+	2. In-built
+	
+	1. User-defined - like factorial, sum, mul, sub ,etc.
+	2. In-built method -there is a predefined class i.e, Math has multiple inbuilt methods like power, 
+	    sqrt,etc
+	 */
 }
