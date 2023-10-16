0d372a37952466e83cfd3c3535f341046971dfee
AbhishekAryanWeb170901 <bittoogoswami@gmail.com>
2023-10-16 23:25:24 +0530

    update prime no

---
+++ 1.Maths/src/practice/d4/$11FindPrimeNo.java
@@ -0,0 +1,16 @@
+package practice.d4;
+
+public class $11FindPrimeNo {
+  static boolean isPrime(int n) {
+	  
+	 for(int i=2;i<n-1;i++) {
+		 if(n%i==0) {
+			 return false;
+		 }
+	 }
+	 return true;
+ }
+  public static void main(String[] args) {
+	System.out.println(isPrime(78));
+}
+}
