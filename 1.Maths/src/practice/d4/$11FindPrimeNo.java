159508732be61fe6d8d91cc96cf5ae56c5f813ec
AbhishekAryanWeb170901 <bittoogoswami@gmail.com>
2023-10-16 23:32:32 +0530

    update prime no using math class

--- 1.Maths/src/practice/d4/$11FindPrimeNo.java
+++ 1.Maths/src/practice/d4/$11FindPrimeNo.java
@@ -3,14 +3,21 @@ package practice.d4;
 public class $11FindPrimeNo {
   static boolean isPrime(int n) {
 	  
-	 for(int i=2;i<n-1;i++) {
-		 if(n%i==0) {
-			 return false;
+	/*
+	 * for(int i=2;i<=n-1;i++) { if(n%i==0) { return false; } } return true; }
+	 */
+	  if(n==2) {
+		  return true;
+	  }
+	  // using Math class find prime no:
+		 for(int i=2;i<=Math.sqrt(n);i++) {
+			 if(n%i==0) {
+				 return false;
+			 }
 		 }
+		 return true;
 	 }
-	 return true;
- }
   public static void main(String[] args) {
-	System.out.println(isPrime(78));
+	System.out.println(isPrime(3));
 }
 }
