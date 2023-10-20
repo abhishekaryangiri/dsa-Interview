238efab03455ffeca83db0aeb7553d49728acb78
AbhishekAryanWeb170901 <bittoogoswami@gmail.com>
2023-10-20 23:17:59 +0530

    update array using scanner class

--- 1.Maths/src/practice/d4/$14ArrayScanClass.java
+++ 1.Maths/src/practice/d4/$14ArrayScanClass.java
@@ -10,8 +10,8 @@ public static void main(String[] args) {
 	marks[0] =sc.nextInt();//physics
 	marks[1] =sc.nextInt();//chemistry
 	marks[2] =sc.nextInt();//maths
-	System.out.println(marks[0]);
-	System.out.println(marks[1]);
-	System.out.println(marks[2]);
+	System.out.println("Physics marks: "+marks[0]);
+	System.out.println("Chemistry marks: "+marks[1]);
+	System.out.println("Maths marks: "+marks[2]);
 }
 }
