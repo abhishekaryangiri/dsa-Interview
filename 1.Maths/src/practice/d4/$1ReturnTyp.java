13acdd4ef1c962c6604be1789cdc7d4866322dff
AbhishekAryanWeb170901 <bittoogoswami@gmail.com>
2023-10-25 23:40:42 +0530

    update bb

--- 1.Maths/src/practice/d4/$1ReturnTyp.java
+++ 1.Maths/src/practice/d4/$1ReturnTyp.java
@@ -7,8 +7,14 @@ public static void helloJava() {
 	System.out.println("Important DSA Problems");
 	System.out.println("Thanks");
 }
+public static void helloWorld() {
+	
+	
+	System.out.println(" Hello World !");
+}
 	public static void main(String[] args) {
 		
 		helloJava();
+		helloWorld();
 	}
 }
