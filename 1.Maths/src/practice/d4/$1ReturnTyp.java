68ffaba07a66e4f55fc4a86ed6bf30d60934839f
AbhishekAryanWeb170901 <bittoogoswami@gmail.com>
2023-10-25 23:37:11 +0530

    update aa

--- 1.Maths/src/practice/d4/$1ReturnTyp.java
+++ 1.Maths/src/practice/d4/$1ReturnTyp.java
@@ -5,6 +5,7 @@ public class $1ReturnTyp {
 public static void helloJava() {
 	
 	System.out.println("Important DSA Problems");
+	System.out.println("Thanks");
 }
 	public static void main(String[] args) {
 		
