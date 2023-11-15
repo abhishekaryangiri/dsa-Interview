6763d0f9d89aa0e269251ffa9644a142f2b4de78
AbhishekAryanWeb170901 <bittoogoswami@gmail.com>
2023-11-15 23:11:16 +0530

    rev arr

--- 1.Maths/src/practice/d4/$15ReverseArray.java
+++ 1.Maths/src/practice/d4/$15ReverseArray.java
@@ -1,11 +1,15 @@
 package practice.d4;
 
+import java.util.Arrays;
+
 public class $15ReverseArray {
 	public static void main(String[] args) {
-        int[] myArray = {1, 2, 3, 4, 5};
+        int[] myArray = {10, 20, 30, 400, 50};
         int[] reversedArray = new int[myArray.length];
         
         for (int i = 0; i < myArray.length; i++) {
-            reversedArray[i] = myArray[myArray.length - 1 - i];}
+            reversedArray[i] = myArray[myArray.length - 1 - i];
+            }
+        System.out.println(Arrays.toString(reversedArray));
 	}
 }
