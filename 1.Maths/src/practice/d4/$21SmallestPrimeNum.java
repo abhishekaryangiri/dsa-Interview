062e011b5104884487aafc3f76a51e431e0ad4ca
AbhishekAryanWeb170901 <bittoogoswami@gmail.com>
2023-11-21 23:44:50 +0530

    l..

--- 1.Maths/src/practice/d4/$21SmallestPrimeNum.java
+++ 1.Maths/src/practice/d4/$21SmallestPrimeNum.java
@@ -1,5 +1,18 @@
 package practice.d4;
 
 public class $21SmallestPrimeNum {
-
+    // Function to check if the number is a prime or not
+    public static boolean isPrime(int n) {
+        if (n < 2) {
+            return false;
+        } else {
+            for (int i = 2; i <= Math.sqrt(n); i++) {
+                if (n % i == 0)
+                    return false;
+            }
+        }
+        return true;
+    }
+    
+    
 }
