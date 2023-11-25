4b894b8e7a2f74fe04060310aeccb3a8f7eea9c6
AbhishekAryanWeb170901 <bittoogoswami@gmail.com>
2023-11-25 17:51:49 +0530

    pal

--- 1.Maths/src/practice/d4/$25Palindrome.java
+++ 1.Maths/src/practice/d4/$25Palindrome.java
@@ -15,5 +15,21 @@ public class $25Palindrome {
     (int num, int dupNum) throws Exception { 
    
     	
-    	
+    	 // base condition to return once we  
+        // move past first digit 
+        if (num == 0) { 
+            return dupNum; 
+        } else { 
+            dupNum = isPalUtil(num / 10, dupNum); 
+        } 
+   
+        // Check for equality of first digit of 
+        // num and dupNum 
+        if (num % 10 == dupNum % 10) { 
+            // if first digit values of num and  
+            // dupNum are equal divide dupNum 
+            // value by 10 to keep moving in sync 
+            // with num. 
+        	
+        	
 }
