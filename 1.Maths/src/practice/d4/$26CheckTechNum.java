26c8827984447af9a8af6e559d43decb8c6672dd
AbhishekAryanWeb170901 <bittoogoswami@gmail.com>
2023-11-26 14:15:37 +0530

    technum

--- 1.Maths/src/practice/d4/$26CheckTechNum.java
+++ 1.Maths/src/practice/d4/$26CheckTechNum.java
@@ -1,5 +1,18 @@
 package practice.d4;
 
 public class $26CheckTechNum {
-
+    // Function to count the
+    // number of digits in a number
+    static int countDigits(int n)
+    {
+        int count = 0;
+        while (n > 0) {
+            n /= 10;
+            count++;
+        }
+        return count;
+    }
+ 
+    // Returns true if n is a tech
+    // number, else false
 }
