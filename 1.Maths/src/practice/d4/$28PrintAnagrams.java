8afd01323d2326aafd02095f5f73c52216796f22
AbhishekAryanWeb170901 <bittoogoswami@gmail.com>
2023-11-28 23:15:33 +0530

    anagrams

--- 1.Maths/src/practice/d4/$28PrintAnagrams.java
+++ 1.Maths/src/practice/d4/$28PrintAnagrams.java
@@ -1,5 +1,19 @@
 package practice.d4;
 
 public class $28PrintAnagrams {
-
+	 // class for each word of duplicate array
+    static class Word {
+        String str; // to store word itself
+        int index; // index of the word in the
+        // original array
+ 
+        // constructor
+        Word(String str, int index)
+        {
+            this.str = str;
+            this.index = index;
+        }
+    }
+    
+    
 }
