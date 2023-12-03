fb7b13bc7dcd38fa082707ee622e0d9d9415dbd0
AbhishekAryanWeb170901 <bittoogoswami@gmail.com>
2023-12-03 22:16:41 +0530

    neg

--- 1.Maths/src/practice/d4/$33CheckNegative.java
+++ 1.Maths/src/practice/d4/$33CheckNegative.java
@@ -3,5 +3,20 @@ import java.util.Scanner;
 import java.util.Arrays;
 
 public class $33CheckNegative {
+	// Define the main method for running the program.
+	  public static void main(String[] args) {
+	    // Initialize an array of numbers.
+	    int[] nums = {1, -2, -5, -4, 3, -6};
+	    // Alternative input arrays:
+	    // int[] nums = {1, 2, 5, -4, 3, -6};
+	    // int[] nums = {1, 2, -5, -4, -3, 6};
+	    System.out.printf("\nOriginal array of numbers:\n" + Arrays.toString(nums));
 
+	    // Call the 'test' method to check for negative dominance in the array.
+	    boolean result = test(nums);
+	    System.out.printf("\nCheck Negative Dominance in the said array! " + result);
+	  }
+
+	  
+	  
 }
