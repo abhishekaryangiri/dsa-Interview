e175389fcb87a7734d3092727a8d73dc6b1c46e7
AbhishekAryanWeb170901 <bittoogoswami@gmail.com>
2023-12-04 23:25:33 +0530

    cp

--- 1.Maths/src/practice/d4/$34CountPrimes.java
+++ 1.Maths/src/practice/d4/$34CountPrimes.java
@@ -18,4 +18,24 @@ public class $34CountPrimes {
         {
             prime[i] = true;
         }
+        // Remaining part of SIEVE
+        prime[0] = false;
+        prime[1] = false;
+        for (int p = 2; p * p <= max_val; p++) 
+        {
+ 
+            // If prime[p] is not changed, then
+            // it is a prime
+            if (prime[p] == true) 
+            {
+ 
+                // Update all multiples of p
+                for (int i = p * 2; i <= max_val; i += p)
+                {
+                    prime[i] = false;
+                }
+            }
+        }
+        
+        
 }
