f266b6cb0a081fa7133782e539c102e281411a22
AbhishekAryanWeb170901 <bittoogoswami@gmail.com>
2023-12-06 22:30:30 +0530

    com elem

--- 1.Maths/src/practice/d4/$36FindCommonElem.java
+++ 1.Maths/src/practice/d4/$36FindCommonElem.java
@@ -30,9 +30,9 @@ public class $36FindCommonElem {
     public static void main(String[] args)
     {
  
-        int[] arr1 = { 1, 5, 10, 20, 40, 80 };
-        int[] arr2 = { 6, 7, 20, 80, 100 };
-        int[] arr3 = { 3, 4, 15, 20, 30, 70, 80, 120 };
+        int[] arr1 = { 1, 5, 10, 20, 40, 50, 80 };
+        int[] arr2 = { 6, 7, 20, 50, 80, 100 };
+        int[] arr3 = { 3, 4, 15, 20, 30, 50, 70, 80, 120 };
         int n1 = arr1.length;
         int n2 = arr2.length;
         int n3 = arr3.length;
@@ -59,7 +59,11 @@ public class $36FindCommonElem {
         System.out.println(
             "Common elements present in arrays are :");
         
-        
+        // Printing the common elements
+        for (int i = 0; i < k[0]; i++) {
+            System.out.print(ans[i] + " ");
+        }
+        System.out.println();       
     
-    
+    }
 }
