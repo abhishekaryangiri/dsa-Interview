fd63f2acd1a062b22ec31818d2e3f7b6855a855d
AbhishekAryanWeb170901 <bittoogoswami@gmail.com>
2023-12-08 22:48:12 +0530

    vow

--- 1.Maths/src/practice/d4/$38CountVowelConso.java
+++ 1.Maths/src/practice/d4/$38CountVowelConso.java
@@ -3,25 +3,24 @@ package practice.d4;
 import java.util.Scanner;
 
 public class $38CountVowelConso {
-	public static void main(String args[]) {
-	System.out.println("Please enter some text"); 
-	Scanner reader = new Scanner(System.in); 
-	String input = reader.nextLine();
-	char[] letters = input.toCharArray(); 
-	int count = 0;
-	for (char c : letters) { 
-		switch (c) 
-		{ 
-	case 'a': 
-		case 'e': 
-			case 'i': 
-				case 'o': 
-					case 'u': 
-						count++; 
-						break; 
+	public static void main(String args[]) { 
+		System.out.println("Please enter some text"); 
+		Scanner reader = new Scanner(System.in); 
+		String input = reader.nextLine(); 
+		char[] letters = input.toCharArray(); 
+		int count = 0; 
+		for (char c : letters) { 
+			switch (c) { 
+			case 'a': 
+				case 'e': 
+					case 'i': 
+						case 'o': 
+							case 'u': 
+								count++; 
+								break; 
+								default: 
+				} 
+			} 
+		System.out.println("Number of vowels in String [" + input + "] is : " + count); 
 	}
-	default:
 	}
-	}
-		
-}
