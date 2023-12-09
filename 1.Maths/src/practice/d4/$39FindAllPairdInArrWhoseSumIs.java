7999b44a8a819ee56e4b375983434d682586b01c
AbhishekAryanWeb170901 <bittoogoswami@gmail.com>
2023-12-09 22:10:16 +0530

    find

--- 1.Maths/src/practice/d4/$39FindAllPairdInArrWhoseSumIs.java
+++ 1.Maths/src/practice/d4/$39FindAllPairdInArrWhoseSumIs.java
@@ -1,5 +1,11 @@
 package practice.d4;
 
 public class $39FindAllPairdInArrWhoseSumIs {
-
+    public static void main(String args[]) {
+        int[] numbers = { 2, 4, 3, 5, 7, 8, 9 };
+        int[] numbersWithDuplicates = { 2, 4, 3, 5, 6, -2, 4, 7, 8, 9 };
+        prettyPrint(numbers, 7);
+        prettyPrint(numbersWithDuplicates, 7);
+    }
+    
 }
