31cc90628e2ade2972d238906759f48b3a764031
AbhishekAryanWeb170901 <bittoogoswami@gmail.com>
2023-12-10 23:37:30 +0530

    find clos..

--- 1.Maths/src/practice/d4/$40Find3ClosetElem.java
+++ 1.Maths/src/practice/d4/$40Find3ClosetElem.java
@@ -12,7 +12,18 @@ int a = 0, b = 0, c = 0;
 // max(abs(A[i]-B[j]),abs(B[j]-C[k]),
 // abs(C[k]-A[i]))
 int ans = Integer.MAX_VALUE;
-
+// Run three nested loop
+for (int i = 0; i < p; i++) {
+    for (int j = 0; j < q; j++) {
+        for (int k = 0; k < r; k++) {
+            int curr
+                = Math.max(Math.abs(A[i] - B[j]),
+                           Math.abs(B[j] - C[k]));
+            int temp = Math.max(
+                curr, Math.abs(C[k] - A[i]));
+            
+            
+            
 
 
 }
