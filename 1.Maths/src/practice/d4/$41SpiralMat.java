ced97459ec738c0fa39528418b041b387e69f525
AbhishekAryanWeb170901 <bittoogoswami@gmail.com>
2023-12-11 23:57:45 +0530

    spi ma

--- 1.Maths/src/practice/d4/$41SpiralMat.java
+++ 1.Maths/src/practice/d4/$41SpiralMat.java
@@ -1,5 +1,22 @@
 package practice.d4;
 
-public class $41SpiralMat {
+import java.util.ArrayList;
 
+public class $41SpiralMat {
+	// Function to print in spiral order
+    public static List<Integer> spiralOrder(int[][] matrix)
+    {
+        List<Integer> ans = new ArrayList()<Integer>();
+ 
+        if (matrix.length == 0)
+            return ans;
+ 
+        int m = matrix.length, n = matrix[0].length;
+        boolean[][] seen = new boolean[m][n];
+        int[] dr = { 0, 1, 0, -1 };
+        int[] dc = { 1, 0, -1, 0 };
+        int x = 0, y = 0, di = 0;
+        
+        
+        
 }
