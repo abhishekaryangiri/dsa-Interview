6ce51a8f324525654c0d06d88812bab41a9a83d6
AbhishekAryanWeb170901 <bittoogoswami@gmail.com>
2023-12-13 23:59:11 +0530

    wgt

--- 2.Array/src/arrayDsa/$1Basic.java
+++ 2.Array/src/arrayDsa/$1Basic.java
@@ -6,10 +6,19 @@ public class $1Basic {
 public static void main(String[] args) {
 	int weight[] = new int[5];
 	Scanner s = new Scanner(System.in);
+	System.out.println("Enter Ramlal's weight");
 	weight[1] = s.nextInt();
+	System.out.println("Enter Shyamlal's weight");
 	weight[2] = s.nextInt();
+	System.out.println("Enter Mohanlal's weight");
 	weight[3] = s.nextInt();
+	System.out.println("Enter Sohanlal's weight");
 	weight[4] = s.nextInt();
+	
+	System.out.println("Ramlal's weight "+ weight[1]);
+	System.out.println("Shyamlal's weight "+ weight[2]);
+	System.out.println("Mohanlal's weight "+ weight[3]);
+	System.out.println("Sohanlal's weight "+ weight[4]);
 	
 }
 }
