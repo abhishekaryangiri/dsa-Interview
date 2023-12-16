17e1cfe8ad2590424f853446e9f3cef79870a79a
AbhishekAryanWeb170901 <bittoogoswami@gmail.com>
2023-12-16 23:58:22 +0530

    read txt

--- interviewQuestion/src/interviewQuestion/ReadTxtBuffer.java
+++ interviewQuestion/src/interviewQuestion/ReadTxtBuffer.java
@@ -1,5 +1,19 @@
 package interviewQuestion;
 
-public class ReadTxtBuffer {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
+public class ReadTxtBuffer {
+	 public static void main(String[] args) {
+	        String fileName = "path/to/your/file.txt";
+	        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+	            String line;
+	            while ((line = br.readLine()) != null) {
+	                System.out.println(line);
+	            }
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        }
+	    }
 }
