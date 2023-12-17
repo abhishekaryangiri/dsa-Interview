f86305b79281e04ec91489804cc37ee96b05ed86
AbhishekAryanWeb170901 <bittoogoswami@gmail.com>
2023-12-17 23:58:28 +0530

    rest api

--- interviewQuestion/src/interviewQuestion/CreateMiniWebServicesRestAPI.java
+++ interviewQuestion/src/interviewQuestion/CreateMiniWebServicesRestAPI.java
@@ -2,6 +2,8 @@ package interviewQuestion;
 
 //1.Step1: crete a springboot project using SI
 //2. import into eclise
+//3.Create a new Java class (e.g., HelloController.java) and annotate it with @RestController. Define a simple REST endpoint.
+
 public class CreateMiniWebServicesRestAPI {
 
 }
