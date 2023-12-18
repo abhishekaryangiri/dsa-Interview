c04e4acc54619f06d7d7a415ad4247a73ec1536a
AbhishekAryanWeb170901 <bittoogoswami@gmail.com>
2023-12-18 23:27:32 +0530

    blo

--- interviewQuestion/src/interviewQuestion/TestThread.java
+++ interviewQuestion/src/interviewQuestion/TestThread.java
@@ -7,7 +7,7 @@ import java.util.concurrent.BlockingQueue;
 //The java.util.concurrent.BlockingQueue is the subinterface of Queue that supports the operations such as waiting for the space availability before inserting a new value or waiting for the queue to become non-empty before retrieving an element from it. Consider the following example.
 
 public class TestThread {
-	public static void main(final String[] arguments) throws InterruptedException {  
+	 public static void main(final String[] arguments) throws InterruptedException {  
 	      BlockingQueue<Integer> queue = new ArrayBlockingQueue<Integer>(10);  
 	  
 	      Insert i = new Insert(queue);  
@@ -18,26 +18,57 @@ public class TestThread {
 	  
 	      Thread.sleep(2000);  
 	   }    
-	
-	try {  
-        int result = random.nextInt(200);  
-        Thread.sleep(1000);  
-        queue.put(result);  
-        System.out.println("Added: " + result);  
-          
-        result = random.nextInt(10);  
-        Thread.sleep(1000);  
-        queue.put(result);  
-        System.out.println("Added: " + result);  
-          
-        result = random.nextInt(50);  
-        Thread.sleep(1000);  
-        queue.put(result);  
-        System.out.println("Added: " + result);  
-     } catch (InterruptedException e) {  
-        e.printStackTrace();  
-     }  
-  }      
-} 
-
+	  
+	  
+	   static class Insert implements Runnable {  
+	      private BlockingQueue<Integer> queue;  
+	  
+	      public Insert(BlockingQueue queue) {  
+	         this.queue = queue;  
+	      }  
+	  
+	      @Override  
+	      public void run() {  
+	         Random random = new Random();  
+	  
+	         try {  
+	            int result = random.nextInt(200);  
+	            Thread.sleep(1000);  
+	            queue.put(result);  
+	            System.out.println("Added: " + result);  
+	              
+	            result = random.nextInt(10);  
+	            Thread.sleep(1000);  
+	            queue.put(result);  
+	            System.out.println("Added: " + result);  
+	              
+	            result = random.nextInt(50);  
+	            Thread.sleep(1000);  
+	            queue.put(result);  
+	            System.out.println("Added: " + result);  
+	         } catch (InterruptedException e) {  
+	            e.printStackTrace();  
+	         }  
+	      }      
+	   }  
+	  
+	   static class Retrieve implements Runnable {  
+	      private BlockingQueue<Integer> queue;  
+	  
+	      public Retrieve(BlockingQueue queue) {  
+	         this.queue = queue;  
+	      }  
+	        
+	      @Override  
+	      public void run() {  
+	           
+	         try {  
+	            System.out.println("Removed: " + queue.take());  
+	            System.out.println("Removed: " + queue.take());  
+	            System.out.println("Removed: " + queue.take());  
+	         } catch (InterruptedException e) {  
+	            e.printStackTrace();  
+	         }  
+	      }  
+	   }
 }
