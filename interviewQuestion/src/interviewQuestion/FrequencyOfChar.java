d5ffc7f0abdda00656bb760ff8abd4d0db07721a
AbhishekAryanWeb170901 <bittoogoswami@gmail.com>
2023-12-27 23:53:03 +0530

    frq

--- interviewQuestion/src/interviewQuestion/FrequencyOfChar.java
+++ interviewQuestion/src/interviewQuestion/FrequencyOfChar.java
@@ -1,5 +1,18 @@
 package interviewQuestion;
 
 public class FrequencyOfChar {
-
+	public static void main(String[] args) {
+		
+		String str = "abhishek aryan amita";
+		char ch = 'a';
+		int frq = 0;
+		
+		for( int i=0;i<str.length();i++) {
+			if( ch == str.charAt(i)) {
+				 //frq = frq+1;
+				++frq;
+			}
+		}
+System.out.println("Frequency of a :  "+ frq);
+	}	
 }
