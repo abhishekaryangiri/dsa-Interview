0ff0584900321a0c722be05dcdf889f988e9f43d
AbhishekAryanWeb170901 <bittoogoswami@gmail.com>
2023-12-28 23:56:20 +0530

    jj

--- interviewQuestion/src/interviewQuestion/PrePostIncrement.java
+++ interviewQuestion/src/interviewQuestion/PrePostIncrement.java
@@ -1,13 +1,24 @@
 package interviewQuestion;
 
 public class PrePostIncrement {
+	public String increment() {
+		int  a=3;
+		int b=6;
+		int t = (a+b);
+		t++;
+		return "sum: " + t;
+		
+	}
 public static void main(String[] args) {	
 	int sum=0;
 	for(int i=0;i<=3;i++) {
 		sum=sum+i;
-
+		//sum++;
+		//++sum;
 	}
-	sum++;
+	//sum++;
 	System.out.println(sum);
+	PrePostIncrement prePostIncrement = new PrePostIncrement();
+	System.out.println(prePostIncrement.increment());
 }
 }
