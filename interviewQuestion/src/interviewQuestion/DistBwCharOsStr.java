4f923821213872c5a5863412377bf1ffc35ef100
AbhishekAryanWeb170901 <bittoogoswami@gmail.com>
2023-12-29 22:18:23 +0530

    dist bw 2 let

---
+++ interviewQuestion/src/interviewQuestion/DistBwCharOsStr.java
@@ -0,0 +1,14 @@
+package interviewQuestion;
+
+public class DistBwCharOsStr {
+
+public static void main(String[] args) {
+	String name = "abhishek aryan";
+	int fn = name.indexOf('a');
+	int ln = name.lastIndexOf('a');
+	
+
+	
+	
+}
+}
