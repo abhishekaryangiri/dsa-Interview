627a7796da7df475ab939e4878226929497eec5d
AbhishekAryanWeb170901 <bittoogoswami@gmail.com>
2024-01-06 23:57:51 +0530

    freq conut uppp

--- interviewQuestion/src/interviewQuestion/FequencyCounterOfNumber.java
+++ interviewQuestion/src/interviewQuestion/FequencyCounterOfNumber.java
@@ -14,7 +14,7 @@ public class FequencyCounterOfNumber {
         return frequency;
     }
 	public static void main(String[] args) {
-        int[] numbers = { 1, 2, 3, 4, 5, 2, 3, 2, 4, 2 };
+        int[] numbers = { 1, 2, 3, 4, 6,6,5, 2, 3,6, 2, 4, 2 };
         int numberToFind = 2;
         int frequency = findFrequency(numbers, numberToFind);
         System.out.println("The frequency of " + numberToFind + " is: " + frequency);
