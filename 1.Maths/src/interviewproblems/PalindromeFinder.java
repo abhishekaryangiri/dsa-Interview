f61591ef0e367e9770e8afc6bbc188e1187d8769
Abhishek Aryan <bittoogoswami@gmail.com>
2024-01-20 23:57:28 +0530

    Update PalindromeFinder.java

--- 1.Maths/src/interviewproblems/PalindromeFinder.java
+++ 1.Maths/src/interviewproblems/PalindromeFinder.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import java.util.List;
 
 public class PalindromeFinder {
+
 public static void main(String[] args) {
     String[] array = {"madam", "car", "aaa", "radar", "dog"};
     List<String> palindromes = findPalindromes(array);
