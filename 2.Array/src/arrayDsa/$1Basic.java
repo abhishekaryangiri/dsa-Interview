b63ace3e6cf145c2780c6cb5bc2c93cbc694d9e3
Abhishek Aryan <bittoogoswami@gmail.com>
2024-02-04 23:57:52 +0530

    Update $1Basic.java

--- 2.Array/src/arrayDsa/$1Basic.java
+++ 2.Array/src/arrayDsa/$1Basic.java
@@ -30,6 +30,7 @@ public static void main(String[] args) {
 	System.out.println("Shyamlal's weight "+ weight[2]);
 
 	System.out.println("Mohanlal's weight "+ weight[3]);
+
 	System.out.println("Sohanlal's weight "+ weight[4]);
 	
 }
