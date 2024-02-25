3ee3174c72bfd92ff45eb70b4d5e2b4b63061e98
Abhishek Aryan <bittoogoswami@gmail.com>
2024-02-25 22:46:26 +0530

    Update PalindromeFinder.java

--- 1.Maths/src/interviewproblems/PalindromeFinder.java
+++ 1.Maths/src/interviewproblems/PalindromeFinder.java
@@ -12,7 +12,6 @@ public static void main(String[] args) {
 
     System.out.println("Possible palindromes in the given array: " + palindromes);
 }
-
 public static List<String> findPalindromes(String[] array) {
     List<String> palindromes = new ArrayList<>();
 
