7908aebd175029a700aa82d3b188c60b50a9cc5c
Abhishek Aryan <bittoogoswami@gmail.com>
2024-03-03 23:37:40 +0530

    Update $1Basic.java

--- 2.Array/src/arrayDsa/$1Basic.java
+++ 2.Array/src/arrayDsa/$1Basic.java
@@ -5,7 +5,6 @@ public class $1Basic {
 public static void main(String[] args) {
 	int weight[] = new int[5];
 	Scanner s = new Scanner(System.in);
-
 	System.out.println("Enter Ramlal's weight");
 
 	weight[1] = s.nextInt();
