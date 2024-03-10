c381d6905779c64769e1ac825faf7539a69301e5
Abhishek Aryan <bittoogoswami@gmail.com>
2024-03-10 23:48:39 +0530

    Update $1Basic.java

--- 2.Array/src/arrayDsa/$1Basic.java
+++ 2.Array/src/arrayDsa/$1Basic.java
@@ -18,7 +18,6 @@ public static void main(String[] args) {
 	weight[4] = s.nextInt();
 
 	System.out.println("Ramlal's weight "+ weight[1]);
-
 	System.out.println("Shyamlal's weight "+ weight[2]);
 	System.out.println("Mohanlal's weight "+ weight[3]);
 
