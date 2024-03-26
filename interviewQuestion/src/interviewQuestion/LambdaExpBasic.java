800028136b2bc6ff7b15c4a0b880af6846372b68
Abhishek Aryan <bittoogoswami@gmail.com>
2024-03-26 22:14:03 +0530

    Update LambdaExpBasic.java

--- interviewQuestion/src/interviewQuestion/LambdaExpBasic.java
+++ interviewQuestion/src/interviewQuestion/LambdaExpBasic.java
@@ -7,7 +7,6 @@ interface MyFunctionalInterface{
 }
 public class LambdaExpBasic {
 public static void main(String[] args) {
-	
 // using lambda exp imp the interface
 	MyFunctionalInterface myFunctionalInterface = (s) -> System.out.println("Hello, "+s);
 // calling the method of the functional interface
