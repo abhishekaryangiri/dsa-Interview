cbe2460371dd3d9bbfb9027afb50fe122c153be4
Abhishek Aryan <bittoogoswami@gmail.com>
2024-04-04 23:53:15 +0530

    Update MaxSubarraySum.java

--- interviewQuestion/src/interviewQuestion/MaxSubarraySum.java
+++ interviewQuestion/src/interviewQuestion/MaxSubarraySum.java
@@ -15,7 +15,6 @@ public class MaxSubarraySum {
     public static void main(String[] args) {
         int[] arr = {-2, -3, 4, -1, -2, 1, 5, -3};
         int result = maxSubarraySum(arr);
-
         System.out.println("Maximum subarray sum: " + result);
     }
 }
