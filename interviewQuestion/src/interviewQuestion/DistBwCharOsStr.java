02bb06e2614f8853a830408785e27aaf9153aaea
Abhishek Aryan <bittoogoswami@gmail.com>
2024-04-05 23:54:09 +0530

    Update DistBwCharOsStr.java

--- interviewQuestion/src/interviewQuestion/DistBwCharOsStr.java
+++ interviewQuestion/src/interviewQuestion/DistBwCharOsStr.java
@@ -5,7 +5,7 @@ public static void main(String[] args) {
 	String name = "abhishek aryan";
 	int fn = name.indexOf('a');
 	int ln = name.lastIndexOf('a');
-	
+
 	if(fn != -1 && ln != -1) {
 		int dist = ln-fn;
 		System.out.println("output: "+ dist);
