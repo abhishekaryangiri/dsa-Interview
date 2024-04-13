9a87a266a59460db4ccd5781abdf24bddeb7ab3d
Abhishek Aryan <bittoogoswami@gmail.com>
2024-04-13 23:22:24 +0530

    Update FequencyCounterOfNumber.java

--- interviewQuestion/src/interviewQuestion/FequencyCounterOfNumber.java
+++ interviewQuestion/src/interviewQuestion/FequencyCounterOfNumber.java
@@ -3,7 +3,6 @@ package interviewQuestion;
 public class FequencyCounterOfNumber {
 	public static int findFrequency(int[] array, int number) {
         int frequency = 0;
-
         // Iterate through the array and count occurrences of the number
         for (int i = 0; i < array.length; i++) {
             if (array[i] == number) {
