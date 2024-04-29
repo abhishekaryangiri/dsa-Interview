e192936e531a9ee3b3ffcf9d24a108dfade1f077
Abhishek Aryan <bittoogoswami@gmail.com>
2024-04-29 23:25:25 +0530

    Update ReadTxtBuffer.java

--- interviewQuestion/src/interviewQuestion/ReadTxtBuffer.java
+++ interviewQuestion/src/interviewQuestion/ReadTxtBuffer.java
@@ -9,7 +9,7 @@ public class ReadTxtBuffer {
 	        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
 	            String line;
 	            while ((line = br.readLine()) != null) {
-	                System.out.println(line);
+	               System.out.println(line);
 	            }
 	        } catch (IOException e) {
 	            e.printStackTrace();
