80b7ca93bae78dfbcbd0cc2af6bff9145cf1da34
Abhishek Aryan <bittoogoswami@gmail.com>
2024-04-30 23:21:11 +0530

    Update CharacterCounter.java

--- interviewQuestion/src/interviewQuestion/CharacterCounter.java
+++ interviewQuestion/src/interviewQuestion/CharacterCounter.java
@@ -3,7 +3,7 @@ public class CharacterCounter {
 	public static void main(String[] args) {
         String input = "Abhishek Aryan";
         countAndPrintCharacterRepetitions(input);
-    }
+     }
     public static void countAndPrintCharacterRepetitions(String input) {
         for (char c : input.toCharArray()) {
             int count = input.length() - input.replace(String.valueOf(c), "").length();
